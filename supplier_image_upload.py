179ea6107700a7727dc38a307d10d9b3a00ef631
Roogry <jodiemantra@gmail.com>
2022-03-12 23:22:40 +0800

    DONE Uploading images to web server

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+import os
+import requests
+
+url = "http://localhost/upload/"
+
+image_path = 'supplier-data/images/'
+jpeg_images = [image_file for image_file in os.listdir(image_path) if image_file.endswith(".jpeg")]
+
+for image in jpeg_images:
+    with open(image_path + image, 'rb') as opened:
+        r = requests.post(url, files={'file': opened})
